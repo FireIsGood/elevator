efc55922103fb9bf9d344965608b074cf5f411ad
FireIsGood <fireisadequate@gmail.com>
2022-10-24 00:21:46 -0700

    Add elevator logic and doors

--- src/components/elevator.svelte
+++ src/components/elevator.svelte
@@ -5,18 +5,34 @@
     { name: "Floor 3", id: 3 },
   ];
 
+  export let currentFloor: Number;
+
   const activateElevator = function (floorId: Number) {
-    const floor = document.getElementById(`floor-${floorId}`);
-    alert(`Going to floor ${floorId}`);
-    floor?.scrollIntoView();
+    if (floorId === currentFloor) return;
+    currentFloor = floorId;
+    const floor: Element = document.querySelector(`#floor-${floorId}`);
+    const doors: Element = document.querySelector(".doors");
+    doors.style.setProperty("--door-percent", "0%");
+    // alert(doors);
+
+    const scrollAction = setTimeout(() => {
+      // floor.scrollIntoView();
+      location.href = `#floor-${floorId}`;
+    }, 500);
+    const openDoors = setTimeout(() => {
+      doors.style.setProperty("--door-percent", "-100%");
+    }, 1200);
   };
 </script>
 
+<div class="doors">
+  <div id="left-door" />
+  <div id="right-door" />
+</div>
 <div class="controller">
   <ul>
     {#each floors as floor, i}
       <li>
-        <a href={`#floor-${floor.id}`}>{floor.name}</a>
         <button
           title={`Go to floor ${floor.id}`}
           on:click={() => activateElevator(floor.id)}
@@ -33,6 +49,17 @@
   button {
     background-color: var(--green-500);
     padding: 0.5ch 1ch;
+
+    &::before {
+      content: "";
+      display: inline-block;
+      width: 1rem;
+      height: 1rem;
+      margin-right: 0.5rem;
+      background-color: var(--green-50);
+      vertical-align: baseline;
+      border-radius: 100vw;
+    }
   }
 
   // Main Styles
@@ -47,6 +74,31 @@
       display: flex;
       flex-direction: column;
       gap: 1rem;
+    }
+  }
+
+  .doors {
+    --door-percent: -100%;
+    z-index: 999;
+    position: fixed;
+    inset: 0;
+    display: grid;
+
+    > div {
+      position: absolute;
+      top: 0;
+      width: 50vw;
+      height: 100vh;
+      transition: left 500ms ease-out, right 500ms ease-out;
+      background-color: var(--elevator-door);
+      border: 0.5rem solid var(--lightblue-900);
+    }
+
+    #left-door {
+      left: var(--door-percent);
+    }
+    #right-door {
+      right: var(--door-percent);
     }
   }
 </style>
